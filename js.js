68779783b7616e4073ada25d706395d26de3d834
fxcx <87233669+fxcx@users.noreply.github.com>
2023-03-01 20:05:10 -0300

    cv

---
+++ js.js
@@ -0,0 +1,8 @@
+// Seleccionar el botón por su clase
+const button = document.querySelector(".button-link");
+
+// Agregar evento "click" al botón
+button.addEventListener("click", function() {
+  // Mostrar una alerta con un mensaje
+  alert("¡Has hecho clic en el botón!");
+});

d638c3f309574edc9c43cf98afa1827da86bbdf8
fxcx <87233669+fxcx@users.noreply.github.com>
2023-03-01 23:07:56 -0300

    cargar form

--- form.js
+++ form.js
@@ -1,24 +1,81 @@
-  // Obtener referencia al formulario
-  const form = document.querySelector('#mi-formulario');
+// Obtener referencia al formulario
+const form = document.querySelector('#mi-formulario');
 
-  // Agregar un evento de escucha para cuando se envíe el formulario
-  form.addEventListener('submit', (event) => {
-    // Prevenir que el formulario se envíe por defecto
-    event.preventDefault();
+// Agregar un evento de escucha para cuando se envíe el formulario
+form.addEventListener('submit', (event) => {
+  // Prevenir que el formulario se envíe por defecto
+  event.preventDefault();
 
-    // Crear un objeto FormData con los datos del formulario
-    const formData = new FormData(form);
+  // Crear un objeto FormData con los datos del formulario
+  const formData = new FormData(form);
 
-    // Agregar el archivo JSON al objeto FormData
-    const fileInput = document.querySelector('#archivo');
-    formData.append('archivo', fileInput.files[0]);
+  // Crear una instancia de la API de Gmail
+  gapi.load('client:auth2', () => {
+    gapi.client.init({
+      apiKey: 'API_KEY',
+      clientId: 'CLIENT_ID',
+      discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest'],
+      scope: 'https://www.googleapis.com/auth/gmail.compose'
+    }).then(() => {
+      // Obtener los datos del formulario
+      const para = formData.get('para');
+      const asunto = formData.get('asunto');
+      const mensaje = formData.get('mensaje');
+      const archivo = formData.get('archivo');
 
-    // Crear una nueva solicitud AJAX
-    const xhr = new XMLHttpRequest();
+      // Crear un mensaje MIME
+      const message = new gapi.client.gmail.users.messages.send({
+        userId: 'me',
+        resource: {
+          raw: makeMessage(para, asunto, mensaje, archivo)
+        }
+      });
 
-    // Configurar la solicitud AJAX
-    xhr.open('POST', form.action);
-
-    // Enviar la solicitud AJAX con el objeto FormData
-    xhr.send(formData);
+      // Enviar el mensaje
+      message.execute((response) => {
+        console.log(response);
+      });
+    });
   });
+});
+
+// Función para crear un mensaje MIME
+function makeMessage(to, subject, body, attachment) {
+  const boundary = 'myboundary';
+
+  const messageParts = [];
+
+  // Parte del mensaje de texto
+  const textPart = [
+    `To: ${to}`,
+    `Subject: ${subject}`,
+    '',
+    body
+  ].join('\n');
+
+  messageParts.push(textPart);
+
+  // Parte del archivo adjunto
+  if (attachment) {
+    const attachmentPart = [
+      `--${boundary}`,
+      `Content-Type: ${attachment.type}`,
+      `Content-Transfer-Encoding: base64`,
+      `Content-Disposition: attachment; filename="${attachment.name}"`,
+      '',
+      attachment.base64,
+      ''
+    ].join('\n');
+
+    messageParts.push(attachmentPart);
+  }
+
+  // Combinar las partes del mensaje
+  const message = messageParts.join(`\n--${boundary}\n`);
+
+  // Codificar el mensaje en base64
+  const encodedMessage = btoa(message);
+
+  // Devolver el mensaje MIME
+  return encodedMessage;
+}
